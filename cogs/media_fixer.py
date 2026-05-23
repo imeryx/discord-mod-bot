1549418ae0e936caca9d2dd8fd8f1f1fdf2b9f34
Tiến Đạt <duongdat@MacBook-Pro-cua-Duong.local>
2026-05-23 21:49:27 +0700

    Doi lai proxy sang ddinstagram

--- cogs/media_fixer.py
+++ cogs/media_fixer.py
@@ -31,7 +31,7 @@ class MediaFixer(commands.Cog):
             
             elif domain == "instagram.com":
                 # Vũ khí 2: Sử dụng zzinstagram (tự động định tuyến máy chủ mượt nhất)
-                fixed_url = clean_url.replace("instagram.com", "zzinstagram.com")
+                fixed_url = clean_url.replace("instagram.com", "ddinstagram.com")
             
             elif domain == "reddit.com":
                 fixed_url = clean_url.replace("reddit.com", "rxddit.com")
